9d20e322a2c0a2ecb974367100775018c16ad59c
Suseendhiran D <suseendhiran.d@f22labs.com>
2022-01-13 01:45:35 +0530

    cors via express

--- index.js
+++ index.js
@@ -4,23 +4,27 @@ var cors = require("cors");
 
 dotenv.config();
 
-app.use(function (req, res, next) {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "http://" + req.headers.host + ":8100"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.setHeader(
+//     "Access-Control-Allow-Origin",
+//     "http://" + req.headers.host + ":8100"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-Requested-With,content-type"
+//   );
+//   next();
+// });
 
 var server = require("http").Server(app);
+
+server.listen(process.env.PORT, () => {
+  console.log("PORT connected", process.env.PORT);
+});
 
 const io = require("socket.io")(server);
 
