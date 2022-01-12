559fe66cf77f3a83caa5f71734f71d906cf01cc6
Suseendhiran D <suseendhiran.d@f22labs.com>
2022-01-13 01:47:02 +0530

    adding headers

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ var cors = require("cors");
 
 dotenv.config();
 
-// app.use(function (req, res, next) {
-//   res.setHeader(
-//     "Access-Control-Allow-Origin",
-//     "http://" + req.headers.host + ":8100"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "X-Requested-With,content-type"
-//   );
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "http://" + req.headers.host + ":8100"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  next();
+});
 
 var server = require("http").Server(app);
 
